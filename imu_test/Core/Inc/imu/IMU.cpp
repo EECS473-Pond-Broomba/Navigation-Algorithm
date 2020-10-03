5ada833552b5515e1038ad97342bd249e70dd68b
cy <cycheung96@gmail.com>
2020-10-03 15:43:54 -0400

    Initial interface for IMU given Adafruit feature list

---
+++ imu_test/Core/Inc/imu/IMU.cpp
@@ -0,0 +1,18 @@
+/*
+ * IMU.cpp
+ *
+ *  Created on: Oct 3, 2020
+ *      Author: cy
+ */
+
+#include <imu/IMU.h>
+
+IMU::IMU() {
+	// TODO Auto-generated constructor stub
+
+}
+
+IMU::~IMU() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ imu_test/Core/Inc/imu/IMU.h
@@ -0,0 +1,38 @@
+/*
+ * IMU.h
+ *
+ *  Created on: Oct 3, 2020
+ *      Author: cy
+ */
+
+#ifndef INC_IMU_IMU_H_
+#define INC_IMU_IMU_H_
+
+class IMU {
+public:
+	IMU();
+	virtual ~IMU();
+
+	// y - Yaw. Vertical axis that we are interested in, should almost always use y
+	// x & z - Pitch and roll
+	enum Axes { x, y, z};
+
+	// Returns Euler angle of boat around input axis
+	// Should almost always pass y in
+	double getOrientation(Axes axis);
+
+	// Returns angular velocity in rad/s
+	// Should almost always pass y in
+	double getAngVel(Axes axis);
+
+	// Returns gravity + linear motion acceleration vector in m/s^2
+	double getTotalAcceleration(Axes axis);
+
+	// Returns linear motion acceleration without gravity in m/s^2
+	double getLinearAcceleration(Axes axis);
+
+private:
+	
+};
+
+#endif /* INC_IMU_IMU_H_ */
