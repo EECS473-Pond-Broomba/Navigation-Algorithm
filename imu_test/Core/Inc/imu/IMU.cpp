15c20711d2ec71d96fb944a55327cebfab8ec4d8
cy <cycheung96@gmail.com>
2020-10-04 22:27:08 -0400

    Added initialization, mode, and backdoor headers

--- imu_test/Core/Inc/imu/IMU.cpp
+++ imu_test/Core/Inc/imu/IMU.cpp
@@ -15,10 +15,33 @@ IMU::~IMU() {
 	// Auto-generated destructor stub
 }
 
-double IMU::getOrientation(Axes axis);
+void IMU::initializeIMU() {
+	// Set mode to NDOF
+	// Set Eular Angles units to degrees or radians
+	// Set Angular Rate units to Dps or Rps
+	// Set Acceleration units to m/s^2 or mg
+}
 
-double IMU::getAngVel(Axes axis);
+double IMU::getOrientation(Axes axis) {
 
-double IMU::getTotalAcceleration(Axes axis);
+}
 
-double IMU::getLinearAcceleration(Axes axis);
+double IMU::getAngVel(Axes axis) {
+
+}
+
+double IMU::getTotalAcceleration(Axes axis) {
+
+}
+
+double IMU::getLinearAcceleration(Axes axis) {
+
+}
+
+void IMU::setMode(IMU_MODE) {
+
+}
+
+void IMU::write8(Registers reg, uint8_t value) {
+
+}
